773526c6c05f5e5e34195672c403cd317a75bc1a
Yghor-Castello <yghorcastello.backend@gmail.com>
2023-02-09 20:23:22 -0300

    Configurando o Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -167,7 +167,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly', #Se o cliente tá autenticado e verificado ele pode fazer tudo 'CRUD'
     )
 }
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
