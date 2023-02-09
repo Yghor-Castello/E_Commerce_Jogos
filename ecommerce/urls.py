a120ce581b6820872f02466afe43cb21b022d4b5
Yghor-Castello <yghorcastello.backend@gmail.com>
2023-02-09 20:24:02 -0300

    Configurando urls do sistema

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from jogos.urls import router
+
 
 urlpatterns = [
+    path('api/v1/', include(router.urls)),# Rotas da API com versionamentos de API
     path('admin/', admin.site.urls),
+    path('auth/', include('rest_framework.urls')),
 ]
